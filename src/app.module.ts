f622a062eb1a3d6ff67c4bfcd5a22115e696059c
hwnii <hwniieo@gmail.com>
2024-06-13 14:14:58 +0900

    쿠키를 활용한 사용자 인증 준비

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,14 @@ import { UserModule } from './user/user.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmConfig } from 'src/configs/typeorm.config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRootAsync({ useClass: TypeOrmConfig }),
     UserModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // Pipe 유효성 검사
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
