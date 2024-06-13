9f648c15ec1768f54ea9f04be1ad26e8ddd0399a
hwnii <hwniieo@gmail.com>
2024-06-13 15:40:19 +0900

    로그인 가드를 사용한 로그인 인증 구현

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,14 @@
-import { Body, Controller, Post, Request, Response } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Request,
+  Response,
+  UseGuards,
+} from '@nestjs/common';
 import { AuthService } from 'src/auth/auth.service';
+import { LoginGuard } from 'src/auth/guards/login.guard';
 import { CreateUserDto } from 'src/user/dto/user.dto';
 
 @Controller('auth')
@@ -27,5 +36,23 @@ export class AuthController {
     } else {
       return res.status(401).send({ message: 'Invalid credentials' });
     }
+  }
+
+  @UseGuards(LoginGuard)
+  @Post('login2')
+  async login2(@Request() req, @Response() res) {
+    if (!req.cookies['login'] && req.user) {
+      res.cookie('login', JSON.stringify(req.user), {
+        httpOnly: true,
+        maxAge: 1000 * 10,
+      });
+    }
+    return res.send({ message: 'login2 success' });
+  }
+
+  @UseGuards(LoginGuard)
+  @Get('guard-test')
+  guardTest() {
+    return '로그인된 때만 이 글이 보입니다.';
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // Pipe 유효성 검사
   app.useGlobalPipes(new ValidationPipe());
+  // 쿠키 파서
+  app.use(cookieParser());
   await app.listen(3000);
 }
 bootstrap();
