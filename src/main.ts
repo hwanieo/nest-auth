4b3eddcb184c5122384a07451bc7554d01481462
hwnii <hwniieo@gmail.com>
2024-06-13 15:59:38 +0900

    패스포트와 세션 준비

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
+import session from 'express-session';
+import passport from 'passport';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,6 +11,17 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
   // 쿠키 파서
   app.use(cookieParser());
+  // 패스포트와 세션
+  app.use(
+    session({
+      secret: 'very-important-secret',
+      resave: false,
+      saveUninitialized: false,
+      cookie: { maxAge: 3600000 },
+    }),
+  );
+  app.use(passport.initialize());
+  app.use(passport.session());
   await app.listen(3000);
 }
 bootstrap();
